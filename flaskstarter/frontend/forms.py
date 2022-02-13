56af1194b54eec96563c05e6f8b8e0098f5bd97c
Tyler Williamson <tylerwil@gmail.com>
2022-02-13 10:17:31 -0700

    Update project to use current version of dependencies and specify version numbers in requirements file

--- flaskstarter/frontend/forms.py
+++ flaskstarter/frontend/forms.py
@@ -3,10 +3,9 @@
 from flask import Markup
 
 from flask_wtf import FlaskForm
-from wtforms import (ValidationError, HiddenField, BooleanField, TextField,
-                     PasswordField, SubmitField, TextAreaField)
-from wtforms.validators import Required, Length, EqualTo, Email
-from wtforms.fields.html5 import EmailField
+from wtforms import (ValidationError, HiddenField, BooleanField, StringField,
+                     PasswordField, SubmitField, TextAreaField, EmailField)
+from wtforms.validators import InputRequired, Length, EqualTo, Email
 
 from ..utils import (NAME_LEN_MIN, NAME_LEN_MAX, PASSWORD_LEN_MIN,
                      PASSWORD_LEN_MAX)
@@ -18,8 +17,8 @@ terms_html = Markup('<a target="blank" href="/terms">Terms of Service</a>')
 
 class LoginForm(FlaskForm):
     next = HiddenField()
-    login = TextField(u'Email', [Required()])
-    password = PasswordField('Password', [Required(),
+    login = StringField(u'Email', [InputRequired()])
+    password = PasswordField('Password', [InputRequired(),
                                           Length(PASSWORD_LEN_MIN,
                                                  PASSWORD_LEN_MAX)])
     remember = BooleanField('Remember me')
@@ -28,12 +27,12 @@ class LoginForm(FlaskForm):
 
 class SignupForm(FlaskForm):
     next = HiddenField()
-    name = TextField(u'Name', [Required(), Length(NAME_LEN_MIN, NAME_LEN_MAX)])
-    email = EmailField(u'Email', [Required(), Email()])
+    name = StringField(u'Name', [InputRequired(), Length(NAME_LEN_MIN, NAME_LEN_MAX)])
+    email = EmailField(u'Email', [InputRequired(), Email()])
     password = PasswordField(u'Password',
-                             [Required(), Length(PASSWORD_LEN_MIN, PASSWORD_LEN_MAX)],
+                             [InputRequired(), Length(PASSWORD_LEN_MIN, PASSWORD_LEN_MAX)],
                              description=u' 6 or more characters.')
-    agree = BooleanField(u'Agree to the ' + terms_html, [Required()])
+    agree = BooleanField(u'Agree to the ' + terms_html, [InputRequired()])
     submit = SubmitField('Sign up')
 
     def validate_email(self, field):
@@ -49,14 +48,14 @@ class RecoverPasswordForm(FlaskForm):
 class ChangePasswordForm(FlaskForm):
     email_activation_key = HiddenField()
     email = HiddenField()
-    password = PasswordField(u'Password', [Required()])
+    password = PasswordField(u'Password', [InputRequired()])
     password_again = PasswordField(u'Password again', [EqualTo('password', message="Passwords don't match")])
     submit = SubmitField('Save')
 
 
 class ContactUsForm(FlaskForm):
-    name = TextField(u'Name', [Required(), Length(max=64)])
-    email = EmailField(u'Your Email', [Required(), Email(), Length(max=64)])
-    subject = TextField(u'Subject', [Required(), Length(5, 128)])
-    message = TextAreaField(u'Your Message', [Required(), Length(10, 1024)])
+    name = StringField(u'Name', [InputRequired(), Length(max=64)])
+    email = EmailField(u'Your Email', [InputRequired(), Email(), Length(max=64)])
+    subject = StringField(u'Subject', [InputRequired(), Length(5, 128)])
+    message = TextAreaField(u'Your Message', [InputRequired(), Length(10, 1024)])
     submit = SubmitField('Submit')
