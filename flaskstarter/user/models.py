2b9cec200c26b7bbb5c6a6a3400303b9477f43f6
Pedram <pedram.yousefi23@gmail.com>
2022-07-19 14:44:19 +0430

    changed the size of _password field to 128

--- flaskstarter/user/models.py
+++ flaskstarter/user/models.py
@@ -61,7 +61,7 @@ class Users(db.Model, UserMixin):
 
     created_time = Column(db.DateTime, default=get_current_time)
 
-    _password = Column('password', db.String(100), nullable=False)
+    _password = Column('password', db.String(128), nullable=False)
 
     def _get_password(self):
         return self._password
