4c3d2c77e05260829608f44894ea4ec07dbd09de
ksh7 <kundansingh.connect@gmail.com>
2022-08-07 10:26:58 +0530

    small fixes

--- tests/test_flaskstarter.py
+++ tests/test_flaskstarter.py
@@ -2,23 +2,22 @@
 
 import pytest
 
-from ..flaskstarter import create_app
-from ..flaskstarter.extensions import db
-from ..flaskstarter.user import Users
+from flask_login import current_user
+from flaskstarter import create_app
+from flaskstarter.extensions import db
+from flaskstarter.user import Users, USER, ACTIVE
+
+@pytest.fixture
+def client():
+    app = create_app()
+
+    app.config["TESTING"] = True
+    app.testing = True
+
+    client = app.test_client()
+    yield client
 
 
-def test_home_page():
-    # TODO
-    return True
-
-
-def test_login_page():
-    # TODO
-
-    return True
-
-
-def test_user_model():
-    # TODO
-
-    return True
+def test_home_page(client):
+    response = client.get("/")
+    assert b"Let\'s start with Python & Flask" in response.data
